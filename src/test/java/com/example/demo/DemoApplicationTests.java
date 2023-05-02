a762e4f9fa60e6935345f9f30f250b780335e04b
Partha Mondal <connect2partha@gmail.com>
2023-05-02 22:57:42 +0530

    Code setup and fixes

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DemoApplication.class)
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private DemoController controller;
+
+    @Test
+    void contextLoads() {
+        Assertions.assertNotNull(controller);
+    }
 
 }
